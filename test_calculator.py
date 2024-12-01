4d6e379b931ee64bd8f1cd346cfe1895aee6e2d7
ig <ig@example.com>
2024-12-01 16:51:12 +0300

    ТЗ

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,9 @@
 from calculator import Calculator
 
+# Задания
+# 1. Разработчику 1 - реализовать функцию умножения
+# 2. Разработчику 2 - реализовать функцию деления
+
 calc = Calculator()
 print(calc.sum(23,37))
 result = calc.sum(2,4,5)
